cb87e18cd110f02d814ca5e9c0c104a00762d35f
Kristofer Krause <kris.krause@gmail.com>
2024-03-13 13:36:12 -0400

    update nav link

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 interface NavigationProps {
     homeActive: boolean,
     aboutActive: boolean
@@ -7,10 +9,10 @@ function Navigation(props: NavigationProps) {
     return (
         <ul className="nav nav-tabs">
             <li className="nav-item">
-                <a className={props.homeActive ? "nav-link active" : "nav-link"} href="/" title="Home">Home</a>
+                <Link to={"/"} className={props.homeActive ? "nav-link active" : "nav-link"} title="Home" >Home</Link>
             </li>
             <li className="nav-item">
-                <a className={props.aboutActive ? "nav-link active" : "nav-link"} href="/about" title="About">About</a>
+                <Link to={"/about"} className={props.aboutActive ? "nav-link active" : "nav-link"} title="About" >About</Link>
             </li>
         </ul>)
 };
